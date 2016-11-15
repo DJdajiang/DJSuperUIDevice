300ba73cc983908263a2e069894f0fb23208f606
刘文江 <464280789@qq.com>
2016-11-15 09:23:26 +0800

    DJSuperUIDeview
    对UIDeview的一个类别，主要是对设备号的一个判断，可以节省小伙伴的时间

---
+++ UIDevice+DJExtension.h
@@ -0,0 +1,15 @@
+//
+//  UIDevice+DJExtension.h
+//  KCKC
+//
+//  Created by 刘文江 on 2016/11/1.
+//  Copyright © 2016年 刘文江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (DJExtension)
+
++ (NSString *)deviceVersion;
+
+@end
